5d331dcef9e2a8901ff5241dc4a52218450ae500
faheelkhan <faheelkhan14@gmail.com>
2017-04-28 00:15:45 +0500

    ids added

--- app/models/bear.js
+++ app/models/bear.js
@@ -2,7 +2,8 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var BearSchema   = new Schema({
-	name: String
+	name: String,
+	age: Number
 });
 
 module.exports = mongoose.model('Bear', BearSchema);
